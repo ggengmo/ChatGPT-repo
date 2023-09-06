332a36124591afc1564b82b0e75d1635ba770ae7
강경모 <142369113+ggengmo@users.noreply.github.com>
2023-09-07 02:08:00 +0900

    Add files via upload

--- spin.js
+++ spin.js
@@ -1,11 +1,11 @@
 let __assign = (this && this.__assign) || function () {
   __assign = Object.assign || function(t) {
-      for (let s, i = 1, n = arguments.length; i < n; i++) {
-          s = arguments[i];
-          for (let p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-              t[p] = s[p];
+    for (let s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (let p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+          t[p] = s[p];
       }
-      return t;
+    return t;
   };
   return __assign.apply(this, arguments);
 };
@@ -40,42 +40,42 @@ let Spinner = /** @class */ (function () {
    * stop() internally.
    */
   Spinner.prototype.spin = function (target) {
-      this.stop();
-      this.el = document.createElement('div');
-      this.el.className = this.opts.className;
-      this.el.setAttribute('role', 'progressbar');
-      css(this.el, {
-          position: this.opts.position,
-          width: 0,
-          zIndex: this.opts.zIndex,
-          left: this.opts.left,
-          top: this.opts.top,
-          transform: "scale(" + this.opts.scale + ")",
-      });
-      if (target) {
-          target.insertBefore(this.el, target.firstChild || null);
-      }
-      drawLines(this.el, this.opts);
-      return this;
+    this.stop();
+    this.el = document.createElement('div');
+    this.el.className = this.opts.className;
+    this.el.setAttribute('role', 'progressbar');
+    css(this.el, {
+      position: this.opts.position,
+      width: 0,
+      zIndex: this.opts.zIndex,
+      left: this.opts.left,
+      top: this.opts.top,
+      transform: "scale(" + this.opts.scale + ")",
+    });
+    if (target) {
+      target.insertBefore(this.el, target.firstChild || null);
+    }
+    drawLines(this.el, this.opts);
+    return this;
   };
   /**
    * Stops and removes the Spinner.
    * Stopped spinners may be reused by calling spin() again.
    */
   Spinner.prototype.stop = function () {
-      if (this.el) {
-          if (typeof requestAnimationFrame !== 'undefined') {
-              cancelAnimationFrame(this.animateId);
-          }
-          else {
-              clearTimeout(this.animateId);
-          }
-          if (this.el.parentNode) {
-              this.el.parentNode.removeChild(this.el);
-          }
-          this.el = undefined;
+    if (this.el) {
+      if (typeof requestAnimationFrame !== 'undefined') {
+        cancelAnimationFrame(this.animateId);
       }
-      return this;
+      else {
+        clearTimeout(this.animateId);
+      }
+      if (this.el.parentNode) {
+        this.el.parentNode.removeChild(this.el);
+      }
+      this.el = undefined;
+    }
+    return this;
   };
   return Spinner;
 }());
@@ -85,7 +85,7 @@ export { Spinner };
 */
 function css(el, props) {
   for (let prop in props) {
-      el.style[prop] = props[prop];
+    el.style[prop] = props[prop];
   }
   return el;
 }
@@ -102,33 +102,33 @@ function drawLines(el, opts) {
   let borderRadius = (Math.round(opts.corners * opts.width * 500) / 1000) + 'px';
   let shadow = 'none';
   if (opts.shadow === true) {
-      shadow = '0 2px 4px #000'; // default shadow
+    shadow = '0 2px 4px #000'; // default shadow
   }
   else if (typeof opts.shadow === 'string') {
-      shadow = opts.shadow;
+    shadow = opts.shadow;
   }
   let shadows = parseBoxShadow(shadow);
   for (let i = 0; i < opts.lines; i++) {
-      let degrees = ~~(360 / opts.lines * i + opts.rotate);
-      let backgroundLine = css(document.createElement('div'), {
-          position: 'absolute',
-          top: -opts.width / 2 + "px",
-          width: (opts.length + opts.width) + 'px',
-          height: opts.width + 'px',
-          background: getColor(opts.fadeColor, i),
-          borderRadius: borderRadius,
-          transformOrigin: 'left',
-          transform: "rotate(" + degrees + "deg) translateX(" + opts.radius + "px)",
+    let degrees = ~~(360 / opts.lines * i + opts.rotate);
+    let backgroundLine = css(document.createElement('div'), {
+      position: 'absolute',
+      top: -opts.width / 2 + "px",
+        width: (opts.length + opts.width) + 'px',
+        height: opts.width + 'px',
+        background: getColor(opts.fadeColor, i),
+        borderRadius: borderRadius,
+        transformOrigin: 'left',
+        transform: "rotate(" + degrees + "deg) translateX(" + opts.radius + "px)",
       });
       let delay = i * opts.direction / opts.lines / opts.speed;
       delay -= 1 / opts.speed; // so initial animation state will include trail
       let line = css(document.createElement('div'), {
-          width: '100%',
-          height: '100%',
-          background: getColor(opts.color, i),
-          borderRadius: borderRadius,
-          boxShadow: normalizeShadow(shadows, degrees),
-          animation: 1 / opts.speed + "s linear " + delay + "s infinite " + opts.animation,
+        width: '100%',
+        height: '100%',
+        background: getColor(opts.color, i),
+        borderRadius: borderRadius,
+        boxShadow: normalizeShadow(shadows, degrees),
+        animation: 1 / opts.speed + "s linear " + delay + "s infinite " + opts.animation,
       });
       backgroundLine.appendChild(line);
       el.appendChild(backgroundLine);
@@ -138,32 +138,32 @@ function parseBoxShadow(boxShadow) {
   let regex = /^\s*([a-zA-Z]+\s+)?(-?\d+(\.\d+)?)([a-zA-Z]*)\s+(-?\d+(\.\d+)?)([a-zA-Z]*)(.*)$/;
   let shadows = [];
   for (let _i = 0, _a = boxShadow.split(','); _i < _a.length; _i++) {
-      let shadow = _a[_i];
-      let matches = shadow.match(regex);
-      if (matches === null) {
-          continue; // invalid syntax
-      }
-      let x = +matches[2];
-      let y = +matches[5];
-      let xUnits = matches[4];
-      let yUnits = matches[7];
-      if (x === 0 && !xUnits) {
-          xUnits = yUnits;
-      }
-      if (y === 0 && !yUnits) {
-          yUnits = xUnits;
-      }
-      if (xUnits !== yUnits) {
-          continue; // units must match to use as coordinates
-      }
-      shadows.push({
-          prefix: matches[1] || '',
-          x: x,
-          y: y,
-          xUnits: xUnits,
-          yUnits: yUnits,
-          end: matches[8],
-      });
+    let shadow = _a[_i];
+    let matches = shadow.match(regex);
+    if (matches === null) {
+      continue; // invalid syntax
+    }
+    let x = +matches[2];
+    let y = +matches[5];
+    let xUnits = matches[4];
+    let yUnits = matches[7];
+    if (x === 0 && !xUnits) {
+      xUnits = yUnits;
+    }
+    if (y === 0 && !yUnits) {
+      yUnits = xUnits;
+    }
+    if (xUnits !== yUnits) {
+      continue; // units must match to use as coordinates
+    }
+    shadows.push({
+      prefix: matches[1] || '',
+      x: x,
+      y: y,
+      xUnits: xUnits,
+      yUnits: yUnits,
+      end: matches[8],
+    });
   }
   return shadows;
 }
@@ -173,9 +173,9 @@ function parseBoxShadow(boxShadow) {
 function normalizeShadow(shadows, degrees) {
   let normalized = [];
   for (let _i = 0, shadows_1 = shadows; _i < shadows_1.length; _i++) {
-      let shadow = shadows_1[_i];
-      let xy = convertOffset(shadow.x, shadow.y, degrees);
-      normalized.push(shadow.prefix + xy[0] + shadow.xUnits + ' ' + xy[1] + shadow.yUnits + shadow.end);
+    let shadow = shadows_1[_i];
+    let xy = convertOffset(shadow.x, shadow.y, degrees);
+    normalized.push(shadow.prefix + xy[0] + shadow.xUnits + ' ' + xy[1] + shadow.yUnits + shadow.end);
   }
   return normalized.join(', ');
 }
@@ -184,7 +184,7 @@ function convertOffset(x, y, degrees) {
   let sin = Math.sin(radians);
   let cos = Math.cos(radians);
   return [
-      Math.round((x * cos + y * sin) * 1000) / 1000,
-      Math.round((-x * sin + y * cos) * 1000) / 1000,
+    Math.round((x * cos + y * sin) * 1000) / 1000,
+    Math.round((-x * sin + y * cos) * 1000) / 1000,
   ];
 }
